0bc8255b055ef6073f4850e1d1f699dd186c7ff4
José Guilherme Vanz <jguilhermevanz@suse.com>
2025-06-05 10:19:43 -0300

    fix: add function to extract env var names.
    Moves the code used to extract the env var names to dedicated
    functions.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-use anyhow::{anyhow, Error, Result};
+use anyhow::{anyhow, Result};
 use guest::prelude::*;
-use k8s_openapi::api::core::v1 as apicore;
+use k8s_openapi::api::core::v1::{self as apicore, Container, EphemeralContainer};
 use kubewarden_policy_sdk::wapc_guest as guest;
 extern crate kubewarden_policy_sdk as kubewarden;
 use kubewarden::{protocol_version_guest, request::ValidationRequest, validate_settings};
@@ -19,9 +19,8 @@ pub extern "C" fn wapc_init() {
     register_function("protocol_version", protocol_version_guest);
 }
 
-fn validate_envvar(settings: &Settings, env_vars: &[apicore::EnvVar]) -> Result<()> {
-    let resource_env_var_names: HashSet<String> =
-        env_vars.iter().map(|envvar| envvar.name.clone()).collect();
+fn validate_envvar(settings: &Settings, env_vars: &[String]) -> Result<()> {
+    let resource_env_var_names: HashSet<String> = env_vars.iter().cloned().collect();
     match settings {
         Settings::ContainsAllOf { envvars } => contains_all_of(envvars, &resource_env_var_names),
         Settings::DoesNotContainAllOf { envvars } => {
@@ -34,50 +33,57 @@ fn validate_envvar(settings: &Settings, env_vars: &[apicore::EnvVar]) -> Result<
     }
 }
 
-fn add_container_name_to_error(err: Error, container_name: String) -> Error {
-    anyhow!("{}: {}", container_name, err)
+fn get_containers_env_vars(containers: &[Container]) -> HashMap<String, Vec<String>> {
+    let mut results = HashMap::new();
+    for container in containers {
+        if let Some(envvar) = &container.env {
+            results.insert(
+                container.name.clone(),
+                envvar.iter().map(|e| e.name.clone()).collect(),
+            );
+        }
+    }
+    results
+}
+
+fn get_ephemeral_containers_env_vars(
+    containers: &[EphemeralContainer],
+) -> HashMap<String, Vec<String>> {
+    let mut results = HashMap::new();
+    for container in containers {
+        if let Some(envvar) = &container.env {
+            results.insert(
+                container.name.clone(),
+                envvar.iter().map(|e| e.name.clone()).collect(),
+            );
+        }
+    }
+    results
 }
 
 fn validate_environment_variables(
     pod: &apicore::PodSpec,
     settings: &settings::Settings,
-) -> Result<()> {
-    let mut results = Vec::<Result<()>>::new();
-    for container in pod.containers.iter() {
-        if let Some(envvar) = &container.env {
-            results.push(
-                validate_envvar(settings, envvar)
-                    .map_err(|err| add_container_name_to_error(err, container.name.clone())),
-            );
-        }
-    }
-    if let Some(init_containers) = &pod.init_containers {
-        for container in init_containers.iter() {
-            if let Some(envvar) = &container.env {
-                results.push(
-                    validate_envvar(settings, envvar)
-                        .map_err(|err| add_container_name_to_error(err, container.name.clone())),
-                );
-            }
-        }
-    }
-    if let Some(ephemeral_containers) = &pod.ephemeral_containers {
-        for container in ephemeral_containers.iter() {
-            if let Some(envvar) = &container.env {
-                results.push(
-                    validate_envvar(settings, envvar)
-                        .map_err(|err| add_container_name_to_error(err, container.name.clone())),
-                );
-            }
-        }
-    }
-    let errors: Vec<String> = results
-        .into_iter()
-        .filter_map(|result| result.err())
+) -> Result<(), Vec<String>> {
+    let mut envvars = get_containers_env_vars(&pod.containers);
+    envvars.extend(get_containers_env_vars(
+        pod.init_containers.as_ref().unwrap_or(&vec![]),
+    ));
+    envvars.extend(get_ephemeral_containers_env_vars(
+        pod.ephemeral_containers.as_ref().unwrap_or(&vec![]),
+    ));
+    let errors = envvars
+        .iter()
+        .filter_map(|(container_name, envvar)| {
+            validate_envvar(settings, envvar)
+                .map_err(|err| anyhow!("{}: {}", container_name, err))
+                .err()
+        })
         .map(|err| err.to_string())
-        .collect();
+        .collect::<Vec<_>>();
+
     if !errors.is_empty() {
-        return Err(anyhow!(errors.join("\n")));
+        return Err(errors);
     }
     Ok(())
 }
@@ -85,48 +91,24 @@ fn validate_environment_variables(
 fn validate(payload: &[u8]) -> CallResult {
     let validation_request: ValidationRequest<settings::Settings> =
         ValidationRequest::new(payload)?;
-    match validation_request.extract_pod_spec_from_object() {
-        Ok(pod_spec) => {
-            if let Some(pod_spec) = pod_spec {
-                return match validate_environment_variables(&pod_spec, &validation_request.settings)
-                {
-                    Ok(_) => kubewarden::accept_request(),
-                    Err(err) => kubewarden::reject_request(Some(err.to_string()), None, None, None),
-                };
-            };
-            // If there is not pod spec, just accept it. There is no data to be
-            // validated.
-            kubewarden::accept_request()
-        }
-        Err(_) => kubewarden::reject_request(
-            Some("Cannot parse validation request".to_string()),
-            None,
-            None,
-            None,
-        ),
+    let pod_spec = validation_request.extract_pod_spec_from_object()?;
+    if let Err(errors) =
+        validate_environment_variables(&pod_spec.unwrap_or_default(), &validation_request.settings)
+    {
+        return kubewarden::reject_request(Some(errors.join(", ")), None, None, None);
     }
+    kubewarden::accept_request()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rstest::rstest;
 
-    use crate::{
-        CONTAINS_ALL_OF_ERROR_MSG, CONTAINS_ANY_OF_ERROR_MSG, DOES_NOT_CONTAIN_ALL_OF_ERROR_MSG,
-        DOES_NOT_CONTAIN_ANY_OF_ERROR_MSG,
-    };
+    use crate::CONTAINS_ANY_OF_ERROR_MSG;
 
-    #[rstest]
-    #[case::contains_any_of(Settings::ContainsAnyOf{envvars: HashSet::from(["a".to_owned(), "b".to_owned()])}, vec!["c".to_owned()], CONTAINS_ANY_OF_ERROR_MSG)]
-    #[case::does_not_contain_any_of(Settings::DoesNotContainAnyOf{envvars: HashSet::from(["a".to_owned(), "b".to_owned()])}, vec!["b".to_owned(), "c".to_owned()],  DOES_NOT_CONTAIN_ANY_OF_ERROR_MSG)]
-    #[case::contains_all_of(Settings::ContainsAllOf{envvars: HashSet::from(["a".to_owned(), "b".to_owned()])}, vec!["a".to_owned(), "c".to_owned()], CONTAINS_ALL_OF_ERROR_MSG)]
-    #[case::does_not_contains_all_of(Settings::DoesNotContainAllOf{envvars: HashSet::from(["a".to_owned(), "b".to_owned()])}, vec!["a".to_owned(), "b".to_owned()], DOES_NOT_CONTAIN_ALL_OF_ERROR_MSG)]
-    fn test_multiple_container_validation(
-        #[case] settings: Settings,
-        #[case] envvars: Vec<String>,
-        #[case] error_message: &str,
-    ) {
+    #[test]
+    fn test_envvar_extraction() {
+        let envvars = ["a".to_owned(), "c".to_owned()];
         let container_envvar = Some(
             envvars
                 .iter()
@@ -136,6 +118,50 @@ mod tests {
                 })
                 .collect::<Vec<_>>(),
         );
+
+        let containers = vec![
+            apicore::Container {
+                name: "test-container1".to_string(),
+                env: container_envvar.clone(),
+                ..Default::default()
+            },
+            apicore::Container {
+                name: "test-container2".to_string(),
+                env: container_envvar.clone(),
+                ..Default::default()
+            },
+        ];
+        let ephemeral_containers = vec![
+            apicore::EphemeralContainer {
+                name: "test-container1".to_string(),
+                env: container_envvar.clone(),
+                ..Default::default()
+            },
+            apicore::EphemeralContainer {
+                name: "test-container2".to_string(),
+                env: container_envvar.clone(),
+                ..Default::default()
+            },
+        ];
+        let mut result = get_containers_env_vars(&containers);
+        assert_eq!(result.get("test-container1").unwrap(), &envvars.to_vec());
+        assert_eq!(result.get("test-container2").unwrap(), &envvars.to_vec());
+        assert_eq!(result.len(), 2);
+        result = get_ephemeral_containers_env_vars(&ephemeral_containers);
+        assert_eq!(result.get("test-container1").unwrap(), &envvars.to_vec());
+        assert_eq!(result.get("test-container2").unwrap(), &envvars.to_vec());
+        assert_eq!(result.len(), 2);
+    }
+
+    #[test]
+    fn test_multiple_container_error_message() {
+        let settings = Settings::ContainsAnyOf {
+            envvars: HashSet::from(["a".to_owned(), "b".to_owned()]),
+        };
+        let container_envvar = Some(Vec::from([apicore::EnvVar {
+            name: "c".to_owned(),
+            ..Default::default()
+        }]));
 
         let pod_spec = &apicore::PodSpec {
             containers: vec![
@@ -177,17 +203,16 @@ mod tests {
             ..Default::default()
         };
 
-        // This test exists to ensure that we will not allow validation of multiple rules at the same time.
-        let error = validate_environment_variables(pod_spec, &settings)
+        // Ensure that all problematic containers are caught and there is an error for each
+        let errors = validate_environment_variables(pod_spec, &settings)
             .expect_err("Expected validation to fail");
-        println!("{}", error);
-        // ensure that all container with invalid environment variables are listed in the error
-        // message
+        assert_eq!(errors.len(), 6, "Expected 6 errors, got {}", errors.len());
         for c in 1..6 {
             assert!(
-                error
-                    .to_string()
-                    .contains(format!("test-container{}: {}", c, error_message).as_str()),
+                errors.iter().any(|errmsg| errmsg.starts_with(&format!(
+                    "test-container{}: {} ",
+                    c, CONTAINS_ANY_OF_ERROR_MSG
+                ))),
                 "Validation error message does not contain expected text"
             );
         }
